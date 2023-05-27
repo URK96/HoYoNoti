8b14a5c0ede4d790f053c4a8b34bc7aa0e0b4a9c
최진혁 <chlwlsgur96@hotmail.com>
2023-05-28 00:39:07 +0900

    Add localization feature

---
+++ HoYoNoti/HoYoNoti/Extensions/LocalizeExtensions.cs
@@ -0,0 +1,24 @@
+﻿using HoYoNoti.Helpers;
+using HoYoNoti.Resources.Strings;
+
+using Microsoft.Extensions.Localization;
+
+namespace HoYoNoti.Extensions;
+
+[ContentProperty(nameof(Key))]
+public class LocalizeExtensions : IMarkupExtension
+{
+    IStringLocalizer<AppStrings> _localizer;
+
+    public string Key { get; set; } = string.Empty;
+
+    public LocalizeExtensions()
+    {
+        _localizer = ServiceHelper.GetService<IStringLocalizer<AppStrings>>();
+    }
+
+    public object ProvideValue(IServiceProvider serviceProvider) => _localizer[Key];
+
+    object IMarkupExtension.ProvideValue(IServiceProvider serviceProvider) => 
+        ProvideValue(serviceProvider);
+}
---
+++ HoYoNoti/HoYoNoti/Helpers/ServiceHelper.cs
@@ -0,0 +1,17 @@
+﻿namespace HoYoNoti.Helpers;
+
+public static class ServiceHelper
+{
+    public static IServiceProvider Current =>
+#if WINDOWS
+            MauiWinUIApplication.Current.Services;
+#elif ANDROID
+            MauiApplication.Current.Services;
+#elif IOS || MACCATALYST
+            MauiUIApplicationDelegate.Current.Services;
+#else
+            null;
+#endif
+
+    public static TService GetService<TService>() => Current.GetService<TService>();
+}
